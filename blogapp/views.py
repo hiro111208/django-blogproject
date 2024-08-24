667ec5348df10d1f3781d1db882bd626bf7cf566
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2024-08-24 17:18:11 +0900

    Implement a functonality of diaplaying a BlogDetail

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import BlogPost
 
 # Create your views here.
@@ -23,3 +23,8 @@ class IndexView(ListView):
     # モデルBlogPostのオブジェクトにorder_by()を適用して
     # BlogPostのレコードを投稿日時の降順で並べ替える
     queryset = BlogPost.objects.order_by('-posted_at')
+
+
+class BlogDetail(DetailView):
+    template_name = 'post.html'
+    model = BlogPost
