229866269db51e0ae524bbb330421b0868cac17d
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2024-09-03 19:17:58 +0900

    Implement a functionality of displaying a post by category

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -6,4 +6,7 @@ app_name = 'blogapp'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('blog-detail/<int:pk>/', views.BlogDetail.as_view(), name='blog_detail'),
+    path('science-list/', views.ScienceView.as_view(), name='science_list'),
+    path('dailylife-list/', views.DailyLifeView.as_view(), name='dailylife_list'),
+    path('music-list/', views.MusicView.as_view(), name='music_list'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -29,3 +29,29 @@ class IndexView(ListView):
 class BlogDetail(DetailView):
     template_name = 'post.html'
     model = BlogPost
+
+
+class ScienceView(ListView):
+    template_name = 'science_list.html'
+    model = BlogPost
+    context_object_name = 'science_records'
+    queryset = BlogPost.objects.filter(
+        category='science').order_by('-posted_at')
+    paginate_by = 2
+
+
+class DailyLifeView(ListView):
+    template_name = 'dailylife_list.html'
+    model = BlogPost
+    context_object_name = 'dailylife_records'
+    queryset = BlogPost.objects.filter(
+        category='dailylife').order_by('-posted_at')
+    paginate_by = 2
+
+
+class MusicView(ListView):
+    template_name = 'music_list.html'
+    model = BlogPost
+    context_object_name = 'music_records'
+    queryset = BlogPost.objects.filter(category='music').order_by('-posted_at')
+    paginate_by = 2
